91eac8ce039915a6568abc2524c32ee08e3a48ff
Thanasak <thanasak@soilfish.co>
2023-11-20 00:48:17 +0700

    Update padding of spinner in home page

--- app/(dashboard)/home/loading.tsx
+++ app/(dashboard)/home/loading.tsx
@@ -3,7 +3,7 @@ import Card from "@/components/Card";
 export default function HomePageLoader() {
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <Card className="px-0 rounded-full">
+      <Card className="p-3">
         <div className="w-16 h-16 animate-spin border-4 border-dashed rounded-full dark:border-violet-400" />
       </Card>
     </div>
